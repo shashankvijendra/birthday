85aede8e46d2a9f9ad97e0e76c42e1b080c27d5a
shashank-vijendra <64251023+shashank-vijendra@users.noreply.github.com>
2021-02-19 16:40:29 +0530

    first project

---
+++ birthdaywishes/setting1.py
@@ -0,0 +1,6 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+def getvalue():
+    data={}
+    data['default']={'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), }
+    return data
